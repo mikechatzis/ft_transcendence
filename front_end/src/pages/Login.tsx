fff277e878f73a4e4b088baf31bdb10ca911ccdf
kacperpucylo <pucylek@wp.pl>
2022-08-29 17:41:53 +0200

    login page

--- front_end/src/pages/Login.tsx
+++ front_end/src/pages/Login.tsx
@@ -1,19 +1,79 @@
 import React from 'react'
 import {useState} from 'react'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 
 const Login: React.FC = () => {
-	const [login, setLogin] = useState('')
-	const [password, setPassword] = useState('')
+	const [values, setValues] = useState({
+		uname:'',
+		password: '',
+		showPassword: false
+	})
+
+	const handleVisibility = () => {
+		setValues({
+			...values,
+			showPassword: !values.showPassword,
+		});
+	};
 
 	return (
-		<form>
-			<div>
-				Login: <input value={login} onChange={(event) => setLogin(event.target.value)} />
-			</div>
-			<div>
-				Password: <input value={"*".repeat(password.length)} onChange={(event) => setPassword(event.target.value)} />
-			</div>
-		</form>
+		<div>
+			<Container maxWidth="sm">
+				<Grid container
+					spacing={2}
+					direction="column"
+					justifyContent="center"
+					style={{minHeight: "100vh"}}
+				>
+					<Paper elevation={2} sx={{padding: 5}}>
+						<form>
+							<Grid container direction="column" spacing={2}>
+								<Grid item>
+									<TextField type="Username"
+										fullWidth
+										label="Enter your username"
+										placeholder="Username"
+										variant="outlined"
+										required />
+								</Grid>
+								<Grid item>
+									<TextField type={values.showPassword ? "text" : "password"}
+									fullWidth
+									label="Password"
+									placeholder="Password"
+									variant="outlined"
+									required
+									InputProps={{
+										endAdornment: (
+											<InputAdornment position="end">
+												<IconButton onClick={handleVisibility}
+												aria-label="toggle password"
+												edge="end">
+													{values.showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+												</IconButton>
+											</InputAdornment>
+										)
+									}} />
+								</Grid>
+								<Grid item>
+									<Button type="submit" fullWidth variant="contained">
+										Sign In
+									</Button>
+								</Grid>
+							</Grid>
+						</form>
+					</Paper>
+				</Grid>
+			</Container>
+		</div>
 	)
 }
 
