318dfbd4ffe7f32b8c436305ed1593803684363b
kacperpucylo <pucylek@wp.pl>
2022-09-09 03:42:22 +0200

    basic chat working, need to fix multpile socket instances on client

--- front_end/src/context/SocketContext.tsx
+++ front_end/src/context/SocketContext.tsx
@@ -1,8 +1,7 @@
 import { createContext } from 'react'
 import io from 'socket.io-client'
 
-const chatSocket = io("http://localhost:3333/chat", {withCredentials: true})
+export const chatSocket = io("http://localhost:3333/chat", {withCredentials: true})
 
-const ChatContext = createContext<any>(null)
+export const ChatContext = createContext<any>(null)
 
-export {ChatContext, chatSocket}
