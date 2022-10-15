d454f6221eb82a0a44e4285aa67bc9133c4b719a
Edgars Kraujins <ekraujin@c2s17d108.42wolfsburg.de>
2022-10-15 21:56:25 +0200

    working single player ping pong game

--- front_end/src/pages/Game.tsx
+++ front_end/src/pages/Game.tsx
@@ -14,44 +14,44 @@ const PLAYER_DOWN = 40
 const PAUSE       = 32
 
 const canv = {
-	height: 700,
-	width: 1400,
+	position: 'relative',
+	height: window.innerHeight/2,
+	width: window.innerWidth/2,
 	backgroundColor: "grey",
-	marginLeft: '10%',
+	margin: 'auto',
 	marginTop: 50,
-	border: '2px solid white'
-};
+	border: '1px solid white'
+} as const;
 
 var opponentStyle = {
-	left: canv.width-20,
-	top: 500,
+	position: 'relative',
+	left: canv.width - 40,
+	top: canv.height/4,
 	height: 100,
 	width: 20,
-	justifyContent: "center",
-	backgroundColor : "blue",
-};
+	backgroundColor : "black",
+} as const;
 
 var playerStyle = {
 	position: 'relative',
+	top: canv.height/4,
 	left: 20,
-	top: 300,
-    height: 100,
-    width: 20,
-    justifyContent: "center",
-    backgroundColor : "black",
-};
+	height: 100,
+	width: 20,
+	backgroundColor : "black",
+} as const;
 
 var ballStyle = {
 	position: 'relative',
 	left: canv.width/2,
-	top: canv.height/3,
+	top: canv.height/4,
 	height: 20,
 	width: 20,
 	display: "block",
 	backgroundColor: "yellow",
-	justifyContent: "center",
 	borderRadius: "100%",
-};
+	
+} as const;
 
 const getStyle = (val: number) => {
 	if(val === OPPONENT)
@@ -66,14 +66,14 @@ const InitState = () =>{
 		player: 0, 
 		opponent: 0,
 
-		ball: canv.width/2 + canv.height/3,
+		ball: canv.width/2 + canv.height/4,
 		ballSpeed: 2,
 		deltaY: -1,
 		deltaX: -1,
 
 		pause: true,
 
-		opponentSpeed: 150,
+		opponentSpeed: 2,
         opponentDir: false,
 
 		playerScore: 0,
@@ -88,13 +88,13 @@ class Pong extends React.Component <any, any>{
 	}
 
 	resetGame = () => {
-		ballStyle = {...ballStyle, top: canv.height/3}
+		ballStyle = {...ballStyle, top: canv.height/4}
 		ballStyle = {...ballStyle, left: canv.width/2}
 		this.setState({ball: ballStyle})
 	}
 
 	touchingEdge = (pos: number) => {
-		if(pos <= 150 || pos >= 750)
+		if(pos <= 200 || pos >= canv.height + 100)
 			return true
 		return false
 	}
@@ -102,15 +102,7 @@ class Pong extends React.Component <any, any>{
 	ballTouchingHorEdge = (pos: number) => {
 		const nextPosY = ballStyle.top + pos;
 
-		if((nextPosY <= -97) || (nextPosY >= canv.height - 120))
-			return true
-		return false
-	}
-
-	ballTouchingVertEdge = (pos: number) => {
-		const nextPosX = ballStyle.left + pos;
-
-		if(nextPosX >= canv.width - 20)
+		if((nextPosY <= -200) || (nextPosY >= canv.height - 220))
 			return true
 		return false
 	}
@@ -121,13 +113,15 @@ class Pong extends React.Component <any, any>{
 
 		if((nextPosX === playerStyle.left + 20) && (nextPosY >= playerStyle.top -125 && nextPosY <= playerStyle.top + 25))
 			return true
+		if((nextPosX === opponentStyle.left - 20) && (nextPosY >= opponentStyle.top - 225 && nextPosY <= opponentStyle.top -125))
+			return true
 		return false
 	}
 
 	moveBoard = (pos: number) => {
 		
 		if(!this.touchingEdge(pos))
-			playerStyle = {...playerStyle, top: pos - 150}
+			playerStyle = {...playerStyle, top: pos - 300}
 		return playerStyle.top
 	}
 
@@ -140,13 +134,13 @@ class Pong extends React.Component <any, any>{
 	}
 
 	isScore = () => {
-		if(ballStyle.left <= 10)
+		if(ballStyle.left <= 10 || ballStyle.left >= canv.width - 20)
 			return true
 		return false
 	}
 
 	bounceBall = () => {
-		if(this.ballTouchingVertEdge(this.state.deltaX) || this.ballTouchPaddle())
+		if(this.ballTouchPaddle())
 		{
 			this.setState({deltaX: -this.state.deltaX});
 			ballStyle = {...ballStyle, left: ballStyle.left - this.state.deltaX}
@@ -156,7 +150,7 @@ class Pong extends React.Component <any, any>{
 			this.setState({deltaY: -this.state.deltaY});
 			ballStyle = {...ballStyle, top: ballStyle.top - this.state.deltaY}
 		}
-		if (!(this.ballTouchingVertEdge(this.state.deltaX) || this.ballTouchPaddle()) && !(this.ballTouchingHorEdge(this.state.deltaY)))
+		if (!(this.ballTouchPaddle()) && !(this.ballTouchingHorEdge(this.state.deltaY)))
 		{
 			ballStyle = {...ballStyle, top: ballStyle.top + this.state.deltaY}
 			ballStyle = {...ballStyle, left: ballStyle.left + this.state.deltaX}
@@ -165,10 +159,20 @@ class Pong extends React.Component <any, any>{
 		this.setState({ball: ballStyle})
 
 		if(this.isScore()){
-			this.setState({opponentScore: this.state.opponentScore+1})
+			if(this.state.deltaX === -1)
+				this.setState({opponentScore: this.state.opponentScore+1})
+			else
+				this.setState({playerScore: this.state.playerScore+1})
 			this.resetGame();
 		}
 	}
+
+	moveOpponent = () => {
+		if((opponentStyle.top + this.state.opponentSpeed <= 10) || (opponentStyle.top + this.state.opponentSpeed >= canv.height - 130))
+			this.setState({opponentSpeed: -this.state.opponentSpeed});
+		opponentStyle = {...opponentStyle, top: opponentStyle.top + this.state.opponentSpeed}
+		this.setState({opponent: opponentStyle.top})
+    }
 
 	componentDidMount() {
 
@@ -176,6 +180,11 @@ class Pong extends React.Component <any, any>{
             if (!this.state.pause)
                 this.bounceBall();
 		}, this.state.ballSpeed);
+
+		setInterval(() => {
+            if (!this.state.pause)
+               this.moveOpponent();
+        }, 4);
 
 		document.addEventListener("mousemove", (event) => {
 			let mousex = event.clientX;
@@ -188,17 +197,14 @@ class Pong extends React.Component <any, any>{
     }
 
 	render() {
-		// const paddle = () => {
-		// 	let val = BACKGROUND;
-		// 	if(this.state.player !== -1 || this.state.opponent !== -1)
-		// 		val = PLAYER;
-		// 	return <Board name={val}/>
-		// }
 		return (
 			<div style={ canv }>
-				<div style={getStyle(OPPONENT)}/>
-				<div style={getStyle(PLAYER)}/>
-				<div style={getStyle(BALL)}/>
+				<div style={{position:'absolute'}}>Player Score: {this.state.playerScore}</div>
+				<div style={{position:'absolute', marginTop: 20}}>AI Score: {this.state.opponentScore}</div>
+				<div style={getStyle(OPPONENT)}></div>
+				<div style={getStyle(PLAYER)}></div>
+				<div style={getStyle(BALL)}></div>
+				
 			</div>
 			
 		)
