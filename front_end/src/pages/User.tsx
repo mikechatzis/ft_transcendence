adb92c5fad895566423acf61355fc97e8ddf868b
Michail Chatzipanagiotou <mchatzip@DESKTOP-UIMC2BG.localdomain>
2022-10-18 17:58:35 +0200

    match history front end

--- front_end/src/pages/User.tsx
+++ front_end/src/pages/User.tsx
@@ -1,11 +1,13 @@
 import Box from "@mui/material/Box"
-import Typography from "@mui/material/Typography"
+// import Typography from "@mui/material/Typography"
 import axios from "axios"
 import { useContext, useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { UrlContext } from "../context/UrlContext"
 import { UserContext } from "../context/UserContext"
 import { Status } from "../enum/status"
+import MatchHistoryUser from "../components/MatchHistoryUser"
+import Notification from '../components/Notification'
 
 const User: React.FC = () => {
 	const [user, setUser] = useState<any>(null)
@@ -13,6 +15,17 @@ const User: React.FC = () => {
 	const baseUrl = useContext(UrlContext)
 	const {context, setContext} = useContext(UserContext)
 	const navigate = useNavigate()
+	const [message, setMessage] = useState<string | null>(null)
+
+	const handleFriend = (user: any) => () => {
+		let friendUser = {...user}
+
+		axios.post(baseUrl + 'users/addFriend', {friend: friendUser.id}, {withCredentials: true}).then(() => {
+			setMessage(`user ${friendUser.name} added to your friendlist`) 
+		}).catch((error) => {
+				console.log(error) 
+			})
+	}
 
 	useEffect(() => {
 		axios.get(baseUrl + `users/user/${name}`, {withCredentials: true}).then((response) => {
@@ -26,27 +39,53 @@ const User: React.FC = () => {
 				navigate("/login")
 			}
 		})
-	}, [baseUrl, name, navigate])
+		const timeout = setTimeout(() => {
+			setMessage('')
+		}, 2000)
 
-	const getStatus = () => {
-		if (user?.status === Status.OFFLINE) {
-			return 'offline'
-		}
-		else if (user?.status === Status.ONLINE) {
-			return 'online'
-		}
-	}
+		return () => clearTimeout(timeout)
+	}, [baseUrl, name, navigate, message])
+
+
+	// const getStatus = () => {
+	// 	if (user?.status === Status.OFFLINE) {
+	// 		return 'offline'
+	// 	}
+	// 	else if (user?.status === Status.ONLINE) {
+	// 		return 'online'
+	// 	}
+	// }
+
 
 	return (
-		<>
-			<Typography variant="h3">
-				Username: {user?.name}
-			</Typography>
-			<Typography variant="h3">
-				Status: {getStatus()}
-			</Typography>
-			<Box component="img" alt="profile" src={baseUrl + `users/${user?.id}/profileImg`} />
-		</>
+		<section className="profile">
+			<Notification message={message} />
+			<Box className="header">
+				<div className="details">
+					<div className="btn btn-one" onClick={handleFriend(user)}>
+						<span className="btn btn-one">Add Friend</span>
+					</div>
+					<div className='emptySpace'></div>
+					<img src={baseUrl + `users/me/profileImg?${Date.now()}`} alt="" className="profile-pic"></img>
+					<h1 className="heading">Username: {user?.name}</h1>
+					<h1 className="sub">Intra Name: {user?.intraName ? user?.intraName : 'not used'}</h1>
+					<div className="stats">
+						<div className="col-4">
+							<h4>Score</h4>
+							<p>{user?.score}</p>
+						</div>
+						<div className="col-4">
+							<h4>Rank</h4>
+							<p>{user?.rank}</p>
+						</div>
+					</div>
+				</div>
+			</Box>
+			<Box maxWidth={'90%'} paddingLeft='10vw' paddingBottom={'2vh'} fontWeight='bold' textAlign={'center'} fontSize='4vh'>
+			Match History
+					<MatchHistoryUser />
+			</Box>
+		</section>
 	)
 }
 
